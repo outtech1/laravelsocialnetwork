12049ba802baf9ceec8ba8fd12c14214c010d06a
ariolab <blariola@gmail.com>
2015-12-11 20:11:48 +0800

    added Search results

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,22 +20,35 @@ Route::get('/', [
 Route::get('/signup',[
 		'uses' => '\Chatty\Http\Controllers\AuthController@getSignup',
 		'as' => 'auth.signup',
+		'middleware' => ['guest'],
 	]);
 
 Route::post('/signup',[
 		'uses' => '\Chatty\Http\Controllers\AuthController@postSignup',
+		'middleware' => ['guest'],
 	]);
 
 Route::get('/signin',[
 		'uses' => '\Chatty\Http\Controllers\AuthController@getSignin',
 		'as' => 'auth.signin',
+		'middleware' => ['guest'],
 	]);
 
 Route::post('/signin',[
 		'uses' => '\Chatty\Http\Controllers\AuthController@postSignin',
+		'middleware' => ['guest'],
 	]);
 
 Route::get('/signout',[
 		'uses' => '\Chatty\Http\Controllers\AuthController@getSignout',
 		'as' => 'auth.signout',
+	]);
+
+/**
+*Search
+*/
+
+Route::get('/search', [
+		'uses' => '\Chatty\Http\Controllers\SearchController@getResults',
+		'as' => 'search.results',
 	]);
